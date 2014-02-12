26b93be1e9d761aad56658078f5222f7fc4765a0
Sam Blackman <sblackman@gmail.com>
2014-02-11 19:25:04 -0600

    sams first ajax div fun

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,8 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+post '/sams_fun_route' do
+  p params[:cool_thing]
+  "<h1>GIANT DIV</h1>"
+end
--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-  
+  $("#square-1").on("click", function(){
+    var toSend = 42 // fill this out with something interesting
+    $.ajax({
+      type: "POST",
+      url: "/sams_fun_route",
+      data: {cool_thing: toSend},
+      success: function(response) {
+        $("#square-1").html(response);
+        $("#square-1").css({width: "300", height: "300"})
+      },
+      dataType: "html"
+    });
+  });
 });
